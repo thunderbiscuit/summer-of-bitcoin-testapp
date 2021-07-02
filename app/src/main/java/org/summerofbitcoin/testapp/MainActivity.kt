cc1536ebb0d849e975a5ebe32c8eb976fb6140ad
thunderbiscuit <thunderbiscuit@protonmail.com>
2021-07-01 21:00:12 -0400

    Add bitcoindevkit dependency

--- app/src/main/java/org/summerofbitcoin/testapp/MainActivity.kt
+++ app/src/main/java/org/summerofbitcoin/testapp/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import org.bitcoindevkit.bdkjni.Lib
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
