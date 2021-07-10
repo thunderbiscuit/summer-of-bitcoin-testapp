ae5005f10edf467524b846a9a8e3e84649ef72fd
thunderbiscuit <thunderbiscuit@protonmail.com>
2021-07-10 14:44:33 -0400

    Build: Migrate Gradle build files to Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+    val kotlin_version = "1.5.20"
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:4.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
